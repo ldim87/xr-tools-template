b2a3a19de7f02c69aff9afbe6064f2af9462515c
Admin <support@1000.menu>
2019-12-18 18:25:08 +0100

    Init

---
+++ src/Template.php
@@ -0,0 +1,147 @@
+<?php
+/**
+ * @author  Dmitriy Lukin <lukin.d87@gmail.com>
+ */
+
+namespace XrTools;
+
+/**
+ * Custom template parts builder
+ */
+class Template {
+
+	private $templatePath;
+
+	private $templatePathPrefix = '';
+
+	private $parts = [];
+
+	private $config;
+
+	private $locale;
+
+	function __construct(Config $config, Locale $locale){
+		$this->config = $config;
+		$this->locale = $locale;
+	}
+	
+	public function setTemplatePath(string $path){
+		$this->templatePath = $path;
+	}
+
+	public function getTemplatePath(): string {
+		return $this->templatePath;
+	}
+
+	public function set($part, $value){
+		$this->parts[$part] = $value;
+	}
+
+	public function setMulti(array $parts){
+		foreach ($parts as $key => $value) {
+			$this->parts[$key] = $value;
+		}
+	}
+	
+	public function get($parts = null){
+		if(!isset($parts)){
+			return $this->parts;
+		}
+		elseif(is_array($parts)){
+			$result = [];
+
+			foreach ($parts as $key) {
+				$result[$key] = $this->parts[$key] ?? null;
+			}
+
+			return $result;
+		}
+		else {
+			return $this->parts[$parts] ?? null;
+		}
+	}
+
+	public function getResult(){
+		// special "null" theme
+		if(empty($this->templatePath)){
+			return $this->get('body');
+		}
+
+		// full theme
+		ob_start();
+		include $this->templatePath;
+		return ob_get_clean();
+	}
+
+
+
+
+	////////////
+	// :TODO: //
+	////////////
+
+
+
+
+	/**
+	 *	:TODO: Переделать (пока что это просто перенесено из general.php)
+	 * 
+	 * Формирует канонический адрес в $GLOBALS['head']['rel_canonical'] для мета тега rel=canonical в шаблонах вывода страниц
+	 * @param  array  $ruleset Set of rules to generate a canonical URL. Processed in this order:
+	 *                         	<ul>
+	 *                         		<li> <strong> url_go_max </strong> int
+	 *                         			- last index of an array $GLOBALS['arr_url_go'] which canonical URL is generated from (indexes greater than url_go_max are ignored)
+	 *                         		<li> <strong> query_count_max </strong> int
+	 *                         			- ignore the whole query string in canonical URL if there are more _GET parameters than this number
+	 *                         	</ul>
+	 * @param  array  $sys     Settings:
+	 *                         	<ul>
+	 *                         		<li> <strong> return_only </strong> bool (off)
+	 *                         			- do not save result to $GLOBALS['head']['rel_canonical'] | 
+	 *                         		<li> <strong> rewrite </strong> bool (off)
+	 *                         			- by default result is saved only if $GLOBALS['head']['rel_canonical'] is empty.
+	 *                         			  This option forces to overwrite the existing canonical URL settings (i.e. for subpage to override it's inherited settings from parent)
+	 *                         	</ul>
+	 * @return string          Generated canonical URL
+	 */
+	function rel_canonical_rules($ruleset = array(), $sys=array()){
+		// результат
+		$rel_canonical = false;
+		
+		// проверяем максимальный уровень arr_url_go
+		if(isset($ruleset['url_go_max'])){
+			$url_go_max = (int) $ruleset['url_go_max'];
+			
+			if(isset($GLOBALS['arr_url_go'][$url_go_max])){
+				$rel_canonical = '';
+				for($i=0; $i<=$url_go_max; $i++){
+					$rel_canonical .= '/'.$GLOBALS['arr_url_go'][$i];
+				}
+			}
+		}
+		
+		// проверяем максимальное кол-во аргументов в запросе (если уже не настроено)
+		if($rel_canonical === false && isset($ruleset['query_count_max'])){
+			$query_count_max = (int) $ruleset['query_count_max'];
+			
+			// учитываем $_GET['go']
+			if(count($_GET)-1 > $query_count_max){
+				$rel_canonical = '/'.$_GET['go'];
+			}
+		}
+		
+		// если нужно только вернуть результат и не нужна авто-настройка параметра в шаблоне
+		// авто-настройка записывается только если атрибут пуст или присутствует настройка 'rewrite'=>true
+		if(empty($sys['return_only']) && (empty($GLOBALS['head']['rel_canonical']) || !empty($sys['rewrite']))){
+			$GLOBALS['head']['rel_canonical'] = $rel_canonical;
+		}
+		
+		return $rel_canonical;
+	}
+
+
+
+
+
+
+}
